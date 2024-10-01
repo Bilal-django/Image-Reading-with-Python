883b549f351bd245b2922f1d6d2d91c99e1f9b3b
Muhammad Bilal Khan <dbilalcoder0.1@gmail.com>
2024-10-01 06:28:10 -0700

    Update imgcolor.py

--- imgcolor.py
+++ imgcolor.py
@@ -1,9 +1,9 @@
 import cv2
 # Reading the image from the file path
-img = cv2.imread("D:\zaid workshop placemeant\clothes pic\men\menpro52.png")
-img = cv2.resize(img,(1200,700))
+img = cv2.imread("D:\zaid workshop placemeant\clothes pic\men\menpro52.png", 0) # 0 show black and white picture and 1 show original
+img = cv2.resize(img,(1200,700)) 
 print(img)
 cv2.imshow("original", img)
-
+# Wait for a key press and close the window
 cv2.waitKey()
 cv2.destroyAllwindows()
